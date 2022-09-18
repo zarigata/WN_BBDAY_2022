44bb54d3beb0c80ccf3014a5a7b6488b17de2084
zarigata <39637452+zarigata@users.noreply.github.com>
2022-09-18 01:55:14 -0300

    INITAIL

---
+++ entradas.h
@@ -0,0 +1,59 @@
+
+void setupConfiguracao()
+{
+const int L1  = 30;
+const int L2  = 28;
+const int L3  = 26;
+const int L4  = 24;
+const int L5  = 26;
+const int L6  = 26;
+const int L7  = 26;
+const int L8  = 26;
+const int L9  = 30;
+const int L10 = 28;
+const int L11 = 26;
+const int L12 = 24;
+const int L13  = 26;
+const int L14  = 26;
+const int L15  = 26;
+const int L16  = 26;
+  //=======================// ENTRADAS
+const int P1  = A0;
+const int P2  = A1;
+const int P3  = A2;
+const int P4  = A3;
+const int P5  = A4;
+const int P6  = A5;
+const int P7  = A6;
+const int P8  = A7;
+const int P9  = A8;
+const int P10 = A9;
+const int P11 = A10;
+const int P12 = A11;
+const int P13 = A12;
+const int P14 = A13;
+const int P15 = A14;
+const int P16 = A15;
+  //====================//
+  int PESS1 = 0;
+  int PESS2 = 0;
+  int PESS3 = 0;
+  int PESS4 = 0;
+  int PESS5 = 0;
+  int PESS6 = 0;
+  int PESS7 = 0;
+  int PESS8 = 0;
+  int PESS9 = 0;
+  int PESS10 = 0;
+  int PESS11 = 0;
+  int PESS12 = 0;
+  int PESS13 = 0;
+  int PESS14 = 0;
+  int PESS15 = 0;
+  int PESS16 = 0;
+  //saidas
+  pinMode(L1, OUTPUT); pinMode(L2, OUTPUT); pinMode(L3, OUTPUT); pinMode(L4, OUTPUT); pinMode(L5, OUTPUT); pinMode(L6, OUTPUT); pinMode(L7, OUTPUT); pinMode(L8, OUTPUT); pinMode(L9, OUTPUT); pinMode(L10, OUTPUT); pinMode(L11, OUTPUT); pinMode(L12, OUTPUT); pinMode(L13, OUTPUT); pinMode(L14, OUTPUT); pinMode(L15, OUTPUT); pinMode(L16, OUTPUT);
+  //entradas
+  pinMode(P1, INPUT); pinMode(P2, INPUT); pinMode(P3, INPUT); pinMode(P4, INPUT); pinMode(P5, INPUT); pinMode(P6, INPUT); pinMode(P7, INPUT); pinMode(P8, INPUT); pinMode(P9, INPUT); pinMode(P10, INPUT); pinMode(P11, INPUT); pinMode(P12, INPUT); pinMode(P13, INPUT); pinMode(P14, INPUT); pinMode(P15, INPUT); pinMode(P16, INPUT);
+
+}
---
+++ leds.ino
@@ -0,0 +1,45 @@
+//⠀⠀⡀⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⡄⠄⠀⠀
+//⠀⠎⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣠⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠷⠓⠀⠀⠱⡳⣿⣿⣿⣿⣿⣿⣿⣿⡿⠳⠁⣰⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠷⠓⠀⠀⠀⢀⠀⠀⠀⠀⠱⡳⣿⣿⣿⠳⠁⠀⠀⠀⣸⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⣿⣿⣿⠷⠓⠀⠀⠀⢀⣌⣾⣿⣯⣎⢈⠀⠀⠀⠀⠱⡳⣯⣎⢈⣬⣾⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⠷⠓⠀⠀⠀⠀⠀⠀⠳⣷⣿⣿⣿⣿⡿⣯⣎⢈⠀⠀⠀⠀⠱⡷⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣈⣾⡿⠳⠁⠀⠀⠱⣿⣯⡎⠀⠀⠀⠀⠐⢹⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣯⣌⠈⠀⠀⠀⢀⣈⣮⡿⠳⠁⠀⠀⠀⣈⣬⡿⠷⠑⠀⠀⠀⢈⣬⣾⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⣿⣿⣯⣌⣮⡿⠳⠁⠀⠀⠀⣈⣬⡿⠷⠑⠀⠀⠀⢈⣬⣾⣿⣿⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⣿⣿⡿⠳⠁⠀⠀⠀⣈⣬⡿⠷⠑⠀⠀⠀⢀⣌⣾⠷⠳⣷⣿⣿⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⡿⠳⠁⠀⠀⠀⣈⣬⣿⠷⠑⠀⠀⠀⢀⣌⣾⡷⠓⠀⠀⠀⠀⠐⠳⣷⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⢍⠈⠀⠀⠀⠀⠳⣷⣿⢌⠈⠀⢀⣌⣾⠿⠓⠁⠀⠀⠀⠀⠀⠀⠀⠀⢀⣘⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⣮⢌⠀⠀⠀⠀⠑⡳⣷⣾⣿⣿⣿⣿⣯⣌⠀⠀⠀⠀⠀⢀⣈⣮⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⠷⠓⡳⣷⣎⢌⠀⠀⠀⠀⠑⡳⣷⣿⡷⠓⠁⠀⠀⢀⣈⣮⣿⣿⣿⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⠟⠀⠀⠀⢀⣈⣿⣿⣿⣎⢌⠀⠀⠀⠀⠁⠀⠀⢀⣈⣮⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⠏⢀⣈⣮⣿⣿⣿⣿⣿⣿⣿⣿⣎⢌⠀⢀⣈⣮⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣰⠀
+//⠀⠇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡰⠀
+//⠀⠀⠠⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠢⠂⠀
+const int L1  = 30; const int L2  = 28; const int L3  = 26; const int L4  = 24; const int L5  = 22; const int L6  = 20; const int L7  = 18; const int L8  = 16; const int L9  = 14; const int L10 = 12; const int L11 = 10; const int L12 = 8; const int L13  = 6; const int L14  = 4; const int L15  = 2; const int L16  = 0;
+//=======================// ENTRADAS
+const int P1  = A0; const int P2  = A1; const int P3  = A2; const int P4  = A3; const int P5  = A4; const int P6  = A5; const int P7  = A6; const int P8  = A7; const int P9  = A8; const int P10 = A9; const int P11 = A10; const int P12 = A11; const int P13 = A12; const int P14 = A13; const int P15 = A14; const int P16 = A15;
+//====================//
+int PESS1 = 0; int PESS2 = 0; int PESS3 = 0; int PESS4 = 0; int PESS5 = 0; int PESS6 = 0; int PESS7 = 0; int PESS8 = 0; int PESS9 = 0; int PESS10 = 0; int PESS11 = 0; int PESS12 = 0; int PESS13 = 0; int PESS14 = 0; int PESS15 = 0; int PESS16 = 0;
+
+//////////////////////////
+const int tempo = 5000;
+const int ligado = 7500;
+/////////////////////////
+
+
+void setup() {
+
+  //saidas
+  pinMode(L1, OUTPUT); pinMode(L2, OUTPUT); pinMode(L3, OUTPUT); pinMode(L4, OUTPUT); pinMode(L5, OUTPUT); pinMode(L6, OUTPUT); pinMode(L7, OUTPUT); pinMode(L8, OUTPUT); pinMode(L9, OUTPUT); pinMode(L10, OUTPUT); pinMode(L11, OUTPUT); pinMode(L12, OUTPUT); pinMode(L13, OUTPUT); pinMode(L14, OUTPUT); pinMode(L15, OUTPUT); pinMode(L16, OUTPUT);
+  //entradas
+  pinMode(P1, INPUT); pinMode(P2, INPUT); pinMode(P3, INPUT); pinMode(P4, INPUT); pinMode(P5, INPUT); pinMode(P6, INPUT); pinMode(P7, INPUT); pinMode(P8, INPUT); pinMode(P9, INPUT); pinMode(P10, INPUT); pinMode(P11, INPUT); pinMode(P12, INPUT); pinMode(P13, INPUT); pinMode(P14, INPUT); pinMode(P15, INPUT); pinMode(P16, INPUT);
+  iniciacao();
+
+}
+
+void loop() {
+
+}
